9ff086825cdd455bfa0de8851ce433509e595131
ricardo <ricardo.yague.ruiz@gmail.com>
2015-11-15 11:22:28 +0100

    eo

---
+++ web/web/src/main/java/com/losquefaltabansoria/configuration/LqfConfiguration.java
@@ -0,0 +1,25 @@
+package com.losquefaltabansoria.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+ 
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.losquefaltabansoria")
+public class LqfConfiguration {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+ 
+        return viewResolver;
+    }
+ 
+}
---
+++ web/web/src/main/webapp/index_bck.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>Hello World!</h1>
+<p>(Deprecated)</p>
+</body>
+</html>
